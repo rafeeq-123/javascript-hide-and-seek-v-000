11254a225cb7a0ee07f16083a46adee73654ba1a
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-07-02 08:29:32 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  var nodes = document.querySelectorAll('#grand-node div');
+  var mostDeep = nodes[nodes.length- 1];
+  return mostDeep
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
